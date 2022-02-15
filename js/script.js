f490d76ae9189123b0142598dbc815b5b95393f2
mac-book <lexx.mg@gmail.com>
2022-02-15 12:34:45 +0300

    sync

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+
 'use strict';
+
+const video = document.querySelector('.main-content__video'),
+      start = document.querySelector('.start-js'),
+      stop = document.querySelector('.stop-js'),
+      time = document.querySelector('.main-content__time');
+
+
+start.addEventListener('click', () => {
+  video.play();
+});
+
+stop.addEventListener('click', () => {
+  video.pause();
+});
+
+video.addEventListener('timeupdate', () => {
+  const currentTime = video.currentTime;
+
+  time.textContent = currentTime;
+
+  if (currentTime > 41.41799667899906 && currentTime < 58.18126962800209) {
+    time.style.color = 'red';
+  } else if (currentTime >= 58.18126962800209) {
+    time.style.color = 'black';
+  }
+});
